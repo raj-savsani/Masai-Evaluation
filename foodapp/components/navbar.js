0d83157abd753a24dcb535b191a2c97fa912cafe
raj-savsani <rrsavsani@gmail.com>
2021-11-08 14:20:40 +0530

    page created

---
+++ foodapp/components/navbar.js
@@ -0,0 +1,11 @@
+function navbar (){
+    return `<div class="home">
+    <a href="./index.html">Home</a>
+</div>
+<div class="two">
+    <a href="./signup.html">sign up</a>
+    <a href="./login.html">login</a>
+</div>`;
+}
+
+export {navbar};
