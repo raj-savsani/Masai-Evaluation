5f4e3d595b55ffd96a18679368b57305d0f9b0f8
raj-savsani <rrsavsani@gmail.com>
2021-11-29 15:23:36 +0530

    working on rest apis

--- Web 12 Backend Evaluation 1/App.js
+++ Web 12 Backend Evaluation 1/App.js
@@ -11,7 +11,7 @@ const connect = () => {
 
 const companySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true},
+    name: { type: String, required: true, unique: true },
     details: { type: String, required: true },
   },
   {
@@ -22,15 +22,47 @@ const companySchema = new mongoose.Schema(
 
 const Company = mongoose.model("company", companySchema);
 
+const skillSchema = new mongoose.Schema(
+  {
+    skill: { type: "string", required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const Skill = mongoose.model("skill", skillSchema);
+
+const citySchema = new mongoose.Schema(
+  {
+    city: { type: "string", required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const City = mongoose.model("city", citySchema);
+
 const jobSchema = new mongoose.Schema(
   {
-    company: {
+    company_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "company",
       required: true,
     },
-    skill: { type: String, required: true},
-    city: { type: String, required: true },
+    skill_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "skill",
+      required: true,
+    },
+    city_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "city",
+      required: true,
+    },
     vacancy: { type: Number, required: true },
     notice_period: { type: String, required: true },
     work_from_home: { type: String, required: true },
@@ -44,26 +76,43 @@ const jobSchema = new mongoose.Schema(
 
 const Job = mongoose.model("job", jobSchema);
 
-app.post('/company', async (req, res) => {
-    try{
-        const company = await Company.create(req.body);
-        res.status(201).send(company);
-    }catch(e){
-        res.status(500).send({message:e.message})
-    }
+app.post("/company", async (req, res) => {
+  try {
+    const company = await Company.create(req.body);
+    res.status(201).send(company);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
 });
 
 
-// app.post('/job', async (req, res) => {
-//     try{
-//         const job = await Job.create(req.body);
-//         res.status(201).send(job);
-//     }catch(e){
-//         res.status(500).send({message:e.message})
-//     }
-// });
+app.post("/skill", async (req, res) => {
+  try {
+    const skill = await Skill.create(req.body);
+    res.status(201).send(skill);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
 
 
+app.post("/city", async (req, res) => {
+  try {
+    const city = await City.create(req.body);
+    res.status(201).send(city);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.post("/job", async (req, res) => {
+  try {
+    const job = await Job.create(req.body);
+    res.status(201).send(job);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
 
 app.listen(1234, async (req, res) => {
   await connect();
