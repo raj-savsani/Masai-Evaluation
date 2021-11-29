6b4ef185c83ea8cee7a8995f808760e6a4fc4602
raj-savsani <rrsavsani@gmail.com>
2021-11-29 15:03:59 +0530

    Model ready all now wroking on rest apis

--- Web 12 Backend Evaluation 1/App.js
+++ Web 12 Backend Evaluation 1/App.js
@@ -11,12 +11,12 @@ const connect = () => {
 
 const companySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    tagline: { type: String, required: true },
+    name: { type: String, required: true, unique: true},
+    details: { type: String, required: true },
   },
   {
     versionKey: false,
-    timestamp: true,
+    timestamps: true,
   }
 );
 
@@ -25,20 +25,45 @@ const Company = mongoose.model("company", companySchema);
 const jobSchema = new mongoose.Schema(
   {
     company: {
-      type: mongoose.Schema.Types.objectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "company",
       required: true,
     },
-    city:{type:String, required: true},
-    vacancy:{type: Number, required: true },
-    notice_period:{type: boolean, required: true},
-    
+    skill: { type: String, required: true},
+    city: { type: String, required: true },
+    vacancy: { type: Number, required: true },
+    notice_period: { type: String, required: true },
+    work_from_home: { type: String, required: true },
+    rating: { type: Number, required: true },
   },
   {
     versionKey: false,
-    timestamp: true,
+    timestamps: true,
   }
 );
+
+const Job = mongoose.model("job", jobSchema);
+
+app.post('/company', async (req, res) => {
+    try{
+        const company = await Company.create(req.body);
+        res.status(201).send(company);
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+});
+
+
+// app.post('/job', async (req, res) => {
+//     try{
+//         const job = await Job.create(req.body);
+//         res.status(201).send(job);
+//     }catch(e){
+//         res.status(500).send({message:e.message})
+//     }
+// });
+
+
 
 app.listen(1234, async (req, res) => {
   await connect();
