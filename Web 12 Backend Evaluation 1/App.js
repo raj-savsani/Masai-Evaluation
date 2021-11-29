92323b13664624c5deb4ec8711ae63db75bcd62e
raj-savsani <rrsavsani@gmail.com>
2021-11-29 14:38:52 +0530

    company sechma & job schema

--- Web 12 Backend Evaluation 1/App.js
+++ Web 12 Backend Evaluation 1/App.js
@@ -1,25 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-const connect = ()=>{
+const connect = () => {
+  return mongoose.connect("mongodb://127.0.0.1:27017/Nakuri");
+};
 
-   return mongoose.connect("mongodb://127.0.0.1:27017/Nakuri");
-} 
+const companySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    tagline: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    timestamp: true,
+  }
+);
 
+const Company = mongoose.model("company", companySchema);
 
-const CompanySchema
-
-
-
-
-
-
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: mongoose.Schema.Types.objectId,
+      ref: "company",
+      required: true,
+    },
+    city:{type:String, required: true},
+    vacancy:{type: Number, required: true },
+    notice_period:{type: boolean, required: true},
+    
+  },
+  {
+    versionKey: false,
+    timestamp: true,
+  }
+);
 
 app.listen(1234, async (req, res) => {
-    await connect();
-    console.log("Listening on port 1234");
-})
+  await connect();
+  console.log("Listening on port 1234");
+});
