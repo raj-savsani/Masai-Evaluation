f192c295b850bf7b776446a070f9594630b949c5
raj-savsani <rrsavsani@gmail.com>
2022-01-17 17:17:52 +0530

    action added for redux

--- job-listing/src/Redux/action.js
+++ job-listing/src/Redux/action.js
@@ -7,7 +7,21 @@ import {
   GET_JOB_ERROR,
 } from "./actionTypes";
 
-
-export const addJobLoading = ()=>{
-    return {type: ADD_JOB_LOADING}
-}
+export const addJobLoading = () => {
+  return { type: ADD_JOB_LOADING };
+};
+export const addJobSuccess = (data) => {
+  return { type: ADD_JOB_SUCCESS, payload: data };
+};
+export const addJobError = () => {
+  return { type: ADD_JOB_ERROR };
+};
+export const getJobLoading = () => {
+  return { type: GET_JOB_LOADING };
+};
+export const getJobSuccess = (data) => {
+  return { type: GET_JOB_SUCCESS, payload: data };
+};
+export const getJobError = () => {
+  return { type: GET_JOB_ERROR };
+};
